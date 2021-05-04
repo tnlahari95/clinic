d94ce5364bccfe0fea5e46e667645bccc3595016
Mogboella <47828317+Mogboella@users.noreply.github.com>
2021-05-04 02:27:31 -0500

    some mods

--- flaskDemo/__init__.py
+++ flaskDemo/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://Lahari:L@hari95@localhost/project1'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://nmogbo:nkemdirim@localhost/patientinfo'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
@@ -16,3 +16,4 @@ from flaskDemo import routes
 from flaskDemo import models
 
 models.db.create_all()
+
--- flaskDemo/models.py
+++ flaskDemo/models.py
@@ -18,10 +18,13 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
+   
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
+
+   
 
 class Appointment(db.Model):
     __table__ = db.Model.metadata.tables['appointment']
